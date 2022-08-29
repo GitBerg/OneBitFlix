8dbf950a7bab3ceed6f5a633cc95a639998d5bf4
Gutemberg Silva Filho <gutemberg.silva@virtus.ufcg.edu.br>
2022-08-29 08:33:58 -0300

    Relationship between models

--- src/adminjs/resources/index.ts
+++ src/adminjs/resources/index.ts
@@ -1,10 +1,15 @@
-import { Category } from './../../models';
+import { Category, Course } from './../../models';
 import { ResourceWithOptions } from "adminjs";
 import { categoryResourceOptions } from './category';
+import { courseResourceOptions } from './course';
 
 export const adminJsResources: ResourceWithOptions[] = [
     {
         resource: Category,
         options: categoryResourceOptions
+    },
+    {
+        resource:Course,
+        options: courseResourceOptions
     }
 ]
