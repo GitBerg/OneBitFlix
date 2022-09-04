95499c10244c192e54f4248ec2862238ea8cc56f
GitBerg <beguinhoboy@hotmail.com>
2022-09-04 20:41:00 -0300

    getting info about the course

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,13 @@
 import { categoriesController } from './controllers/categoriesController';
 import express from "express";
+import { coursesController } from './controllers/coursesController';
 
 const router = express.Router()
 
 router.get('/categories', categoriesController.index)
 router.get('/categories/:id', categoriesController.show)
+
+router.get('/courses/:id', coursesController.show)
+
 
 export { router }
