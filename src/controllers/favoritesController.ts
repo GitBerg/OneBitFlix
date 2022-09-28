11d0cd192410d813f582c12c3b52bd00dc6f80f7
Gutemberg Silva Filho <gutemberg.silva@virtus.ufcg.edu.br>
2022-09-28 17:25:14 -0300

    deleting a favorite

--- src/controllers/favoritesController.ts
+++ src/controllers/favoritesController.ts
@@ -16,7 +16,7 @@ export const favoritesController = {
             }
         }
     },
-    
+
     save: async (req: AutheticatedRequest, res: Response) =>{
         const userId = req.user!.id
         const {courseId} = req.body
@@ -24,6 +24,20 @@ export const favoritesController = {
         try {
             const favorite = await favoriteService.create(userId, Number(courseId))
             return res.status(201).json(favorite)
+        } catch (error) {
+            if (error instanceof Error) {
+                return res.status(400).json({ message: error.message })
+            }
+        }
+    },
+
+    delete: async(req: AutheticatedRequest, res:Response) => {
+        const userId = req.user!.id
+        const courseId = req.params.id
+
+        try {
+            await favoriteService.delete(userId, Number(courseId))
+            return res.status(204).send()
         } catch (error) {
             if (error instanceof Error) {
                 return res.status(400).json({ message: error.message })
